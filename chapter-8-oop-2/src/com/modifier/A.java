d14c77dd90dc0dcbd116b84c0b4637fa39d6e049
WeiMin <1772429411@qq.com>
2023-05-17 19:27:31 +0800

    包

---
+++ chapter-8-oop-2/src/com/modifier/A.java
@@ -0,0 +1,4 @@
+package com.modifier;
+
+public class A {
+}
---
+++ chapter-8-oop-2/src/com/use/Test.java
@@ -0,0 +1,9 @@
+package com.use;
+
+import com.xiaoming.Dog;
+
+public class Test {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+    }
+}
